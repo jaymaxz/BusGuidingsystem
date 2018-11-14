9deaa4a70a8b5cd7ad0f2baf94dfbcc56d9dd850
jaymaxz <jagathmax2@gamil.com>
2018-11-14 22:42:55 +0530

    enter as updated

--- app/src/main/java/com/uok/se/busguidingsystem/EnterAs.java
+++ app/src/main/java/com/uok/se/busguidingsystem/EnterAs.java
@@ -13,7 +13,31 @@ public class EnterAs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_as);
         final Button button = findViewById(R.id.btnDriver);
+        final Button btnPas = findViewById(R.id.btnPessenger);
+        final Button btnOwn = findViewById(R.id.btnOwner);
+        final Button btnOth = findViewById(R.id.btnOther);
         button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent map = new Intent(EnterAs.this,  MapsActivity.class);
+                startActivity(map);
+            }
+        });
+        btnPas.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent map = new Intent(EnterAs.this,  MapsActivity.class);
+                startActivity(map);
+            }
+        });
+        btnOwn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent map = new Intent(EnterAs.this,  MapsActivity.class);
+                startActivity(map);
+            }
+        });
+        btnPas.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
                 Intent map = new Intent(EnterAs.this,  MapsActivity.class);
